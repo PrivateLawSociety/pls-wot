6804dcdde84462baacf7246b9acb4c205ddb0f2e
marcusmmmz <marcusizabel230@gmail.com>
2024-10-12 17:01:17 -0300

    feat: Create ratings table

---
+++ src/routes/rate/+page.svelte
@@ -0,0 +1,184 @@
+<script lang="ts">
+	import { ReviewEvent } from "$lib";
+	import { finalizeEvent } from "nostr-tools"
+
+	import { getPublicKey } from "nostr-tools/pure";
+	import { relayList, relayPool } from "$lib/nostr";
+	import { decode } from "nostr-tools/nip19";
+
+	function parseSecKey(str: string) {
+		try {
+			const nip19 = decode(str)
+
+			if (nip19.type == "nsec") return nip19.data
+		} catch {
+			try {
+				const buf = hexStringToBuffer(str)
+				
+				if (buf.length !== 32) return;
+
+				return buf
+			} catch {}
+		}
+	}
+
+	function parsePubKey(str: string) {
+		try {
+			const nip19 = decode(str)
+
+			if (nip19.type == "npub") return hexStringToBuffer(nip19.data)
+		} catch {
+			try {
+				const buf = hexStringToBuffer(str)
+				
+				if (buf.length !== 32) return;
+
+				return buf
+			} catch {}
+		}
+	}
+
+	function hexStringToBuffer(str: string) {
+		return Uint8Array.from(toHex(str))
+	}
+
+	function toHex(hexString: string) {
+		let hex = []
+		
+		let i = 0
+		while (i < hexString.length) {
+			hex[i/2] = parseInt(
+				hexString[i] + hexString[i+1],
+				16
+			)
+
+			i += 2
+		}
+
+		return hex
+	}
+
+	function numberToHex(i: number) {
+		return ('0' + i.toString(16)).slice(-2);
+	}
+	
+	function bufToHexString(buf: Uint8Array) {
+		return buf.reduce((memo, i) => memo + numberToHex(i), '');
+	}
+
+	interface Review {
+		from: string,
+		to: string,
+		score: boolean,
+		businessAlreadyDone: boolean,
+		description: string
+	}
+	
+	let mySecretKey = ""
+	let otherPersonPubKey = ""
+	let ratingDescription = ""
+	let score: boolean
+	let businessAlreadyDone: boolean
+
+	async function handleSubmit() {
+		if (ratingDescription.length >= 1000) 
+			return alert("2000 characters is the max for a rating description")
+		
+		if (score == undefined || businessAlreadyDone == undefined) 
+			return alert("You forgot to fill some checkbox")
+		
+		const mySecKey = parseSecKey(mySecretKey)
+		if (!mySecKey) return
+			alert("Invalid secret key")
+
+		const myPubkey = getPublicKey(mySecKey)
+
+		const otherPubKey = parsePubKey(otherPersonPubKey)
+		if (!otherPubKey) return
+			alert("Invalid public key")
+
+		let rating: Review = {
+			from: myPubkey,
+			to: bufToHexString(otherPubKey),
+			score,
+			businessAlreadyDone,
+			description: ratingDescription,
+		};
+
+		const event = finalizeEvent({
+			content: JSON.stringify(rating),
+			created_at: Math.floor(Date.now() / 1000),
+			kind: ReviewEvent,
+			tags: []
+		}, mySecKey)
+
+		console.log(event)
+
+		// TODO: Sign review, let user download it, and disabled 'send to nostr' button
+		// 'send to nostr' should be in a parameterized replaceable event (but just do ephemeral for now)
+	
+		relayPool.publish(relayList, event)
+
+		alert("event published")
+	}
+</script>
+
+
+<form 
+	on:submit={(e)=>{
+		e.preventDefault()
+		handleSubmit()
+	}}
+	class="flex flex-col items-center gap-4 pt-4"
+>	
+	<a
+		class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+		href="https://nostrtool.com/"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Generate keys here
+	</a>
+	
+	<label class="flex flex-col w-1/2">
+		Your secret key (hex)
+		<input class="border-2" bind:value={mySecretKey} type="text" >
+	</label>
+	
+	<label class="flex flex-col w-1/2">
+		Other person pubkey (hex)
+		<input class="border-2" bind:value={otherPersonPubKey} type="text" >
+	</label>
+
+	<div class="flex flex-col w-1/2">
+		<p>What rating do you give to this person?</p>
+		<label>
+			<input bind:group={score} value={true} type="radio">
+			positive
+		</label>
+		<label>
+			<input bind:group={score} value={false} type="radio">
+			negative
+		</label>
+	</div>
+
+	<div class="flex flex-col w-1/2">
+		<p>Have you ever done business with this person?</p>
+		<label>
+			<input bind:group={businessAlreadyDone} value={true} type="radio">
+			yes
+		</label>
+		<label>
+			<input bind:group={businessAlreadyDone} value={false} type="radio">
+			no
+		</label>
+	</div>
+	
+	<label class="flex flex-col w-1/2">
+		Rating description
+		<textarea rows=6 class="border-2" bind:value={ratingDescription}></textarea>
+	</label>
+
+	<button type="submit" class="px-2 py-2 border-2">Create rating</button>
+</form>
+
---
+++ src/routes/table/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { ReviewEvent } from "$lib";
+	import { relayList, relayPool } from "$lib/nostr";
+	import { npubEncode } from "nostr-tools/nip19";
+	import { onMount } from "svelte";
+
+	let ratings = [
+		{
+			from: "test from",
+			to: "test to",
+			date: Date.now(),
+			score: false,
+			businessAlreadyDone: true,
+			description: "test description"
+		},
+	]
+
+	onMount(() => {
+		relayPool.subscribeMany(relayList, [{kinds: [ReviewEvent]}], {
+			onevent(e) {
+				try {
+					const c = JSON.parse(e.content)
+					ratings = [...ratings, {
+						from: npubEncode(c.from),
+						to: npubEncode(c.to),
+						date: e.created_at*1000,
+						score: c.score,
+						businessAlreadyDone: c.businessAlreadyDone,
+						description: c.description,
+					}]
+				} catch {}
+			}
+		})
+	})
+
+</script>
+
+<div class="flex items-center flex-col gap-8">
+	<h1 class="text-2xl font-bold">Ratings table (Currently using ephemeral events)</h1>
+
+	<table>
+		<tr>
+			<th>Rater Nostr Key</th>
+			<th>Rated Nostr Key</th>
+			<th>Date</th>
+			<th>Rating</th>
+			<th>Had <br> business</th>
+			<th>Description</th>
+		</tr>
+		{#each ratings as rating}
+			<tr>
+				<td>{rating.from}</td>
+				<td>{rating.to}</td>
+				<td>
+					{new Date(rating.date).toLocaleDateString()}
+					<br>
+					{new Date(rating.date).toLocaleTimeString()}
+					</td>
+				<td>{rating.score ? "👍" : "👎"}</td>
+				<td>{rating.businessAlreadyDone ? "👍" : "👎"}</td>
+				<td>{rating.description}</td>
+			</tr>
+		{/each}
+	</table>
+</div>
+
+<style>
+	table, th, td {
+		border: 1px solid;
+	}
+
+	td {
+		text-align: center;
+	}
+
+	table {
+		width: 80%;
+	}
+</style>
