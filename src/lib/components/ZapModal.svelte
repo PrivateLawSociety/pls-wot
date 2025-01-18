68592e0e21bf6c95b9f17b7836e710f4104ec842
TheMhv <mhv.geral@gmail.com>
2025-01-18 13:09:20 -0300

    Refact/Feat: Create ZapModal component

---
+++ src/lib/components/ZapModal.svelte
@@ -0,0 +1,189 @@
+<script lang="ts">
+	import { createInvoice } from '$lib/nostr';
+	import QRCode from 'qrcode';
+	import { writable } from 'svelte/store';
+
+	let textInvoiceInput;
+
+	const modalState = writable({
+		isOpen: false,
+		destination: '',
+		message: '',
+		amount: 21,
+		invoice: '',
+		invoiceQR: ''
+	});
+
+	type ModalState = {
+		isOpen: boolean;
+		destination: string;
+		message: string;
+		amount: number;
+		invoice: string;
+		invoiceQR: string;
+	};
+
+	const initialState: ModalState = {
+		isOpen: false,
+		destination: '',
+		message: '',
+		amount: 21,
+		invoice: '',
+		invoiceQR: ''
+	};
+
+	export function openModal(receiver: string) {
+		modalState.set({
+			...initialState,
+			isOpen: true,
+			destination: receiver
+		});
+	}
+
+	function closeModal() {
+		modalState.set(initialState);
+	}
+
+	async function handleSubmit() {
+		try {
+			const { destination, message, amount } = $modalState;
+			const data = await createInvoice(destination, message, amount);
+			const invoiceQR = await QRCode.toDataURL(data.pr);
+
+			modalState.update((state) => ({
+				...state,
+				invoice: data.pr,
+				invoiceQR
+			}));
+		} catch (error) {
+			console.error('Error creating invoice:', error);
+			closeModal();
+			alert('Error trying to create invoice');
+		}
+	}
+
+	async function copyInvoice() {
+		try {
+			await navigator.clipboard.writeText($modalState.invoice);
+			alert('Copied!');
+		} catch (error) {
+			console.error('Failed to copy invoice:', error);
+		}
+	}
+</script>
+
+<div
+	class="fixed inset-0 z-10 flex items-center justify-center {$modalState.isOpen ? '' : 'hidden'}"
+>
+	<button class="fixed inset-0 cursor-default bg-black/30 backdrop-blur-sm" on:click={closeModal}
+	></button>
+
+	<div class="relative mx-auto w-full max-w-2xl p-4">
+		<div class="relative rounded-lg bg-gray-700 shadow-xl">
+			<div class="flex items-center justify-between border-b border-gray-600 p-4">
+				<h3 class="text-xl font-semibold text-white">Send Zap</h3>
+
+				<button
+					type="button"
+					class="inline-flex h-8 w-8 items-center justify-center rounded-lg text-sm text-white transition-colors hover:bg-gray-600"
+					on:click={closeModal}
+					aria-label="Close modal"
+				>
+					<svg
+						class="h-3 w-3"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 14 14"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+						/>
+					</svg>
+				</button>
+			</div>
+
+			<div class="p-4">
+				{#if $modalState.invoiceQR}
+					<div class="space-y-8">
+						<div class="flex items-center justify-center">
+							<img
+								src={$modalState.invoiceQR}
+								alt="QR code for lightning invoice"
+								width="256"
+								height="256"
+								class="rounded-lg"
+							/>
+						</div>
+
+						<div class="flex items-center justify-center gap-4 px-4">
+							<input
+								bind:this={textInvoiceInput}
+								type="text"
+								class="w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white focus:ring-2"
+								value={$modalState.invoice}
+								readonly
+								aria-label="Lightning invoice"
+							/>
+							<button
+								type="button"
+								class="rounded-lg bg-orange-500 px-5 py-2 text-white transition-colors hover:bg-orange-600 focus:ring-2 focus:ring-orange-300"
+								on:click={copyInvoice}
+							>
+								Copy
+							</button>
+						</div>
+
+						<button
+							type="button"
+							class="w-full rounded-lg bg-gray-800 px-5 py-2 text-white transition-colors hover:bg-gray-600 focus:ring-2"
+							on:click={() => modalState.update((state) => ({ ...state, invoiceQR: '' }))}
+						>
+							Return
+						</button>
+					</div>
+				{:else}
+					<form on:submit|preventDefault={handleSubmit} class="space-y-6">
+						<input type="hidden" name="destination" bind:value={$modalState.destination} required />
+
+						<div class="space-y-2">
+							<label for="amount" class="block text-sm text-white"> Amount: </label>
+							<input
+								id="amount"
+								name="amount"
+								type="number"
+								bind:value={$modalState.amount}
+								class="w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white focus:ring-2"
+								min="1"
+								required
+							/>
+						</div>
+
+						<div class="space-y-2">
+							<label for="message" class="block text-sm text-white"> Message: </label>
+							<textarea
+								id="message"
+								name="message"
+								bind:value={$modalState.message}
+								rows="3"
+								class="w-full resize-none rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white focus:ring-2"
+								placeholder="Leave a message..."
+							></textarea>
+						</div>
+
+						<button
+							type="submit"
+							class="w-full rounded-lg bg-orange-500 px-5 py-2 text-sm text-white transition-colors hover:bg-orange-600 focus:ring-2 focus:ring-orange-300"
+						>
+							Create Invoice
+						</button>
+					</form>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -1,39 +1,11 @@
 <script lang="ts">
-	import { ReviewEvent } from "$lib";
-	import { createInvoice, relayList, relayPool } from "$lib/nostr";
-	import { npubEncode } from "nostr-tools/nip19";
-	import { onMount } from "svelte";
-	import QRCode from "qrcode";
+	import { ReviewEvent } from '$lib';
+	import { relayList, relayPool } from '$lib/nostr';
+	import { npubEncode } from 'nostr-tools/nip19';
+	import { onMount } from 'svelte';
+	import ZapModal from '$lib/components/ZapModal.svelte';
 
-	let modalIsOpen = false;
-	let zapDestination: HTMLInputElement;
-	
-	let invoice = ""
-	let invoiceQR = ""
-
-	function openZapModal(receiver: string) {
-		zapDestination.value = receiver;
-		modalIsOpen = true;
-	}
-
-	function handleSubmit(e) {
-		const formData = new FormData(e.target)
-
-		const data: {
-			destination?: string;
-			message?: string;
-			amount?: number;
-		} = {};
-		for (let field of formData) {
-			const [key, value] = field;
-			data[key] = value;
-		}
-		
-		createInvoice(data.destination, data.message, data.amount).then(async (data) => {
-			invoice = data.pr;
-			invoiceQR = await QRCode.toDataURL(invoice)
-		});
-	}
+	let ZapModalComponent: ZapModal;
 
 	let ratings: {
 		from: string;
@@ -42,103 +14,44 @@
 		score: boolean;
 		businessAlreadyDone: boolean;
 		description: string;
-	}[] = []
-	
-	$: ratings.sort((a, b) => b.date - a.date)
+	}[] = [];
+
+	$: ratings.sort((a, b) => b.date - a.date);
 
 	onMount(() => {
-		relayPool.subscribeMany(relayList, [{
-			kinds: [ReviewEvent],
-			"#l": ["pls-wot-rating"]
-		}], {
-			onevent(e) {
-				try {
-					const c = JSON.parse(e.content)
-					ratings = [...ratings, {
-						from: npubEncode(c.from),
-						to: npubEncode(c.to),
-						date: e.created_at*1000,
-						score: c.score,
-						businessAlreadyDone: c.businessAlreadyDone,
-						description: c.description,
-					}]
-				} catch {}
+		relayPool.subscribeMany(
+			relayList,
+			[
+				{
+					kinds: [ReviewEvent],
+					'#l': ['pls-wot-rating']
+				}
+			],
+			{
+				onevent(e) {
+					try {
+						const c = JSON.parse(e.content);
+						ratings = [
+							...ratings,
+							{
+								from: npubEncode(c.from),
+								to: npubEncode(c.to),
+								date: e.created_at * 1000,
+								score: c.score,
+								businessAlreadyDone: c.businessAlreadyDone,
+								description: c.description
+							}
+						];
+					} catch {}
+				}
 			}
-		})
-	})
+		);
+	});
 </script>
 
-<div class="{modalIsOpen ? '' : 'hidden'} fixed top-0 right-0 left-0 z-10 justify-center items-center w-full h-full backdrop-blur-sm">
-	<div class="flex items-center justify-center min-h-screen">
-		<div class="relative mx-auto p-4 w-full max-w-2xl z-20">
-			<div class="relative bg-gray-700 rounded-lg">
-				<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-					<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-						Send Zap
-					</h3>
-	
-					<button type="button" on:click={() => {modalIsOpen = false}} class="inline-flex justify-center items-center text-sm text-white rounded-lg w-8 h-8">
-						<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-						</svg>
-					</button>
-				</div>
+<ZapModal bind:this={ZapModalComponent} />
 
-				<div class="p-4">
-					{#if invoiceQR !== ""}
-						<div class="space-y-8">
-							<div class="flex items-center justify-center">
-								<img src={invoiceQR} alt="qrcode" width="256" height="256">
-							</div>
-	
-							<div class="flex items-center justify-center gap-4 px-4">
-								<input type="text" class="bg-gray-700 text-sm text-white border border-gray-600 rounded-lg w-full p-2.5" value={invoice} readonly />
-								<!-- <span class="text-wrap truncate max-w-96">{invoice}</span> -->
-								<button type="button" class="text-white bg-gray-800 rounded-lg px-5 py-2"
-								on:click={(e) => {
-									e.target.innerHTML = "Copied";
-									navigator.clipboard.writeText(invoice)
-								}}>
-									Copy
-								</button>
-							</div>
-
-							<div class="flex items-center justify-center">
-								<button type="button" class="text-white bg-gray-800 rounded-lg px-5 py-2 w-full"
-								on:click={() => (invoiceQR = "")}>
-									Return
-								</button>
-							</div>
-						</div>
-					{:else}
-						<form on:submit|preventDefault={handleSubmit}>
-							<input bind:this={zapDestination} name="destination" type="hidden" required>
-	
-							<div class="mb-5 space-y-2">
-								<label for="amount" class="text-sm text-white mb-2">Amount:</label>
-								<input id="amount" name="amount" type="number" class="bg-gray-700 text-sm text-white placeholder-gray-400 border border-gray-600 rounded-lg w-full p-2.5" value=21 min=1 required />
-							</div>
-	
-							<div class="mb-5 space-y-2">
-								<label for="message" class="text-sm text-white">Message:</label>
-								<textarea id="message" name="message" rows="3" class="bg-gray-700 text-sm text-white placeholder-gray-400 border border-gray-600 rounded-lg w-full p-2.5" placeholder="Leave a message..."></textarea>
-							</div>
-	
-							<div class="flex items-center justify-center">
-								<button type="submit" class="text-sm text-white bg-orange-500 rounded-lg px-5 py-2 w-full">
-									Create Invoice
-								</button>
-							</div>
-						</form>
-					{/if}
-				</div>
-				
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="flex items-center flex-col gap-8">
+<div class="flex flex-col items-center gap-8">
 	<h1 class="text-2xl font-bold">Ratings table (Currently using replaceable events)</h1>
 
 	<table>
@@ -147,7 +60,7 @@
 			<th>Rated Nostr Key</th>
 			<th>Date</th>
 			<th>Rating</th>
-			<th>Had <br> business</th>
+			<th>Had <br /> business</th>
 			<th>Description</th>
 			<th>Zap</th>
 		</tr>
@@ -157,16 +70,14 @@
 				<td>{rating.to}</td>
 				<td>
 					{new Date(rating.date).toLocaleDateString()}
-					<br>
+					<br />
 					{new Date(rating.date).toLocaleTimeString()}
-					</td>
-				<td>{rating.score ? "👍" : "👎"}</td>
-				<td>{rating.businessAlreadyDone ? "👍" : "👎"}</td>
+				</td>
+				<td>{rating.score ? '👍' : '👎'}</td>
+				<td>{rating.businessAlreadyDone ? '👍' : '👎'}</td>
 				<td>{rating.description}</td>
 				<td>
-					<button on:click={() => openZapModal(rating.from)}>
-						Send Zap
-					</button>
+					<button on:click={() => ZapModalComponent.openModal(rating.from)}> Send Zap </button>
 				</td>
 			</tr>
 		{/each}
@@ -174,7 +85,9 @@
 </div>
 
 <style>
-	table, th, td {
+	table,
+	th,
+	td {
 		border: 1px solid;
 	}
 
