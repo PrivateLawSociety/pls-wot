2b213bd63d2dbbaa74bf7b50ac39527b292c3714
TheMhv <mhv.geral@gmail.com>
2025-03-14 09:20:02 -0300

    Fix/Refact: Use svelte/store to save profiles metadata

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -159,13 +159,12 @@ public key: ${pubkey}`
 	};
 })();
 
-export const profilesMetadata: Event[] = [];
+export const profilesMetadata = writable<Record<string, Event>>({});
 export const getProfileMetadata = async (publicKey: string): Promise<Event | null> => {
 	try {
-		const getFromCache = profilesMetadata.find((profile) => profile.pubkey == publicKey);
-
-		if (getFromCache) {
-			return getFromCache;
+		const profiles = get(profilesMetadata);
+		if (profiles[publicKey]) {
+			return profiles[publicKey];
 		}
 
 		const metadataEvent = await relayPool.get(relayList, {
@@ -175,7 +174,10 @@ export const getProfileMetadata = async (publicKey: string): Promise<Event | nul
 		});
 
 		if (metadataEvent) {
-			profilesMetadata.push(metadataEvent);
+			profilesMetadata.update((profiles) => ({
+				...profiles,
+				[publicKey]: metadataEvent
+			}));
 		}
 
 		return metadataEvent;
