0378e3d2c0fea4532e13520e13809df391ff8e6c
kiuusai <kiuusai@protonmail.com>
2025-04-30 13:49:29 -0300

    Beautiful toasts instead of a boring alert to warn about the copy to clipboard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
 	import '../app.css';
+	import { ToastContainer, FlatToast } from "svelte-toasts";
 </script>
+
+<ToastContainer
+	placement="top-right"
+	theme="dark"
+	duration={3000}
+	showProgress={true}
+	let:data={data}
+>
+	<FlatToast {data} />
+</ToastContainer>
 
 <div class="flex flex-col h-screen">
 	<slot />
