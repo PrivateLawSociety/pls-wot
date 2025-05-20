10b1f98fd438fc71b8fa81965275bab773563020
kiuusai <kiuusai@protonmail.com>
2025-05-20 07:42:03 -0300

    Removing hyphens from rating text

--- src/lib/components/GraphRatingText.svelte
+++ src/lib/components/GraphRatingText.svelte
@@ -2,6 +2,6 @@
 	export let text = '';
 </script>
 
-<div class="inline-block max-w-sm hyphens-auto whitespace-break-spaces break-words">
+<div class="inline-block max-w-sm hyphens-none whitespace-break-spaces break-words">
 	{text}
 </div>
