9ebf2bc32f8136ae7a99fa39a901ad8447546112
TheMhv <mhv.geral@gmail.com>
2025-08-12 02:00:56 -0300

    Feat: Create Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { getProfileMetadata, parseProfileFromJsonString, type ProfileType } from '$lib/nostr';
+	import { npubEncode } from 'nostr-tools/nip19';
+	import ProfileAvatar from './ProfileAvatar.svelte';
+	import { nostrAuth } from '$lib/nostr';
+
+	let pubkey = $nostrAuth?.pubkey ?? '';
+	let username = '';
+
+	let profileMetadata: ProfileType | undefined = undefined;
+
+	async function loadProfile(pubkey: string) {
+		if (!pubkey) return;
+
+		const event = await getProfileMetadata(pubkey);
+
+		if (!event) return;
+		if (!pubkey) return;
+
+		profileMetadata = parseProfileFromJsonString(event?.content || '{}', {
+			npub: npubEncode(pubkey),
+			pubkey: pubkey
+		});
+		username = profileMetadata.name ?? 'No name';
+	}
+
+	if ($nostrAuth?.pubkey) {
+		loadProfile(pubkey);
+	}
+</script>
+
+<div class="flex w-full justify-center p-5">
+	<div class="flex w-3/4 items-center">
+		<div class="w-full flex-col">
+			<a href="/">
+				<button
+					type="button"
+					class="rounded border border-gray-600 bg-gray-700 px-2 py-1 text-white transition-colors hover:bg-orange-600 focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500"
+				>
+					Back to home
+				</button>
+			</a>
+		</div>
+
+		{#if $nostrAuth?.pubkey}
+			<div class="flex items-center gap-4 px-2">
+				<ProfileAvatar source={profileMetadata?.picture} alt={username} />
+				<div class="font-medium text-white">
+					<div>{username || ''}</div>
+					<div class="group relative">
+						<span class="block max-w-24 text-sm text-gray-400">
+							{`${profileMetadata?.npub.slice(0, 5)}...${profileMetadata?.npub.slice(-5)}`}
+						</span>
+
+						<span
+							class="absolute right-0 top-full z-10 hidden whitespace-nowrap rounded-md border border-white bg-gray-800 p-2 text-sm text-white group-hover:block"
+						>
+							{profileMetadata?.npub}
+						</span>
+					</div>
+				</div>
+			</div>
+		{/if}
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,7 @@
 	import '../app.css';
 	import { ToastContainer, FlatToast } from 'svelte-toasts';
 	import { page } from '$app/stores';
-	import LoginPerson from '$lib/components/LoginPerson.svelte';
-	import { nostrAuth } from '$lib/nostr';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
 <ToastContainer placement="top-right" theme="dark" duration={3000} showProgress={true} let:data>
@@ -12,26 +11,7 @@
 
 <div class="flex h-screen flex-col">
 	{#if $page.route.id != '/'}
-		<div class="flex w-full justify-center p-5">
-			<div class="flex w-3/4 items-center">
-				<div class="w-full flex-col">
-					<a href="/">
-						<button
-							type="button"
-							class="rounded border border-gray-600 bg-gray-700 px-2 py-1 text-white transition-colors hover:bg-orange-600 focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500"
-						>
-							Back to home
-						</button>
-					</a>
-				</div>
-
-				<div class="w-full flex-col flex justify-end items-end">
-					{#if $nostrAuth?.pubkey}
-						<LoginPerson pubkey={$nostrAuth.pubkey} />
-					{/if}
-				</div>
-			</div>
-		</div>
+		<Header/>
 	{/if}
 
 	<slot />
