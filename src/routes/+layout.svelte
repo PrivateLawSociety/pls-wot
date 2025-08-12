656c4a3422345666272e3771d23a1722a8bb8674
TheMhv <mhv.geral@gmail.com>
2025-08-12 01:29:17 -0300

    Feat: Add 'back to home' button into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import { ToastContainer, FlatToast } from 'svelte-toasts';
+	import { page } from '$app/stores';
 </script>
 
 <ToastContainer placement="top-right" theme="dark" duration={3000} showProgress={true} let:data>
@@ -8,5 +9,18 @@
 </ToastContainer>
 
 <div class="flex h-screen flex-col">
+	{#if $page.route.id != '/'}
+		<div class="p-5">
+			<a href="/">
+				<button
+					type="button"
+					class="rounded border border-gray-600 bg-gray-700 px-2 py-1 text-white transition-colors hover:bg-orange-600 focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500"
+				>
+					Back to home
+				</button>
+			</a>
+		</div>
+	{/if}
+
 	<slot />
 </div>
